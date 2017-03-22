f617a9401df9c0c5c85b02e9e42f6055d4ed2572
渡邉 俊光 <watanabe_toshimitsu@cyberagent.co.jp>
2017-03-22 15:40:42 +0900

    first commit

---
+++ Assets/CharacterMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+	public float speed = 3f;
+	void Update ()
+	{
+		var pos = transform.position;
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			pos.x += speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			pos.x -= speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			pos.y += speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			pos.y -= speed * Time.deltaTime;
+		}
+		transform.position = pos;
+	}
+}
---
+++ Assets/RotationTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTest : MonoBehaviour
+{
+	public Transform target;
+	public float radius = 3f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		var pos = target.position;
+		var quat = Quaternion.AngleAxis (pos.x / (radius * 2f * Mathf.PI) * 360f, Vector3.back);
+		transform.position = quat * new Vector3 (0f, pos.y + radius, 0f);
+		transform.rotation = quat;
+	}
+}
